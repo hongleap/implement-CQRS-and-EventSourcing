192365e2833d25965b4ce4d6d43653b1ffebed80
hongleap <hongleap0000@gmail.com>
2026-03-11 10:36:20 +0700

    fixed changes files

---
+++ microservices/account-query-service/src/main/java/hongleap/account_query_service/AccountQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package hongleap.account_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountQueryServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/account-query-service/src/test/java/hongleap/account_query_service/AccountQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hongleap.account_query_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
