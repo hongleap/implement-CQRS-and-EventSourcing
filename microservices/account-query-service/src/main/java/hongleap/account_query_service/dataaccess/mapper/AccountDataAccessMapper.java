4055301e0e0d3ba385c4d63bbd744ee37bd715e3
hongleap <hongleap0000@gmail.com>
2026-03-12 08:42:11 +0700

    add: @Mapping annotation

--- microservices/account-query-service/src/main/java/hongleap/account_query_service/dataaccess/mapper/AccountDataAccessMapper.java
+++ microservices/account-query-service/src/main/java/hongleap/account_query_service/dataaccess/mapper/AccountDataAccessMapper.java
@@ -3,10 +3,12 @@ package hongleap.account_query_service.dataaccess.mapper;
 import hongleap.account_query_service.dataaccess.entity.AccountEntity;
 import hongleap.account_query_service.domain.entity.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountDataAccessMapper {
 
+    @Mapping(constant = "true", target = "new")
     AccountEntity accountToAccountEntity(Account account);
 
     Account accountEntityToAccount(AccountEntity accountEntity);
